8e5239f2558b700ebe6255ada742336202f2a19c
ctoloczko <celina.toloczko@gmail.com>
2024-09-08 13:34:35 +0200

    added links in footer

--- src/App.js
+++ src/App.js
@@ -51,8 +51,31 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="text-center">
-          This project was coded by Celina Toloczko-Mastalerz and is
-          open-sourced on GitHub and hosted on Netlify.
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/celinatoloczko/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Celina Toloczko-Mastalerz
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/ctoloczko/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://vocabventures.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
         </footer>
       </div>
     </div>
